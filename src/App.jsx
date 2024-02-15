353baf54e0260e2d8b2041b5bcda3aa78b86bd52
Arian <Arianbod@outlook.com>
2024-02-15 18:28:55 +0300

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,42 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	About,
+	Cocktail,
+	Error,
+	HomeLayout,
+	Landing,
+	Newsletter,
+} from './pages/';
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomeLayout />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: '/Cocktail',
+				element: <Cocktail />,
+			},
+			{
+				path: '/about',
+				element: <About />,
+			},
+			{
+				path: '/Error',
+				element: <Error />,
+			},
+
+			{
+				path: '/Newsletter',
+				element: <Newsletter />,
+			},
+		],
+	},
+]);
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+	return <RouterProvider router={router}>mixmaster starter</RouterProvider>;
 };
 export default App;
