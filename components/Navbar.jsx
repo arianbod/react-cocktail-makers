0ac78c2649a435b161fff929fc436d7c50c91235
Arian <Arianbod@outlook.com>
2024-02-15 18:28:43 +0300

    created

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav>
+			<div className='nav-center'>
+				<span className='logo'>MixMaster</span>
+				<div className='nav-links'>
+					<NavLink
+						to='/'
+						className='nav-link'>
+						Home
+					</NavLink>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
