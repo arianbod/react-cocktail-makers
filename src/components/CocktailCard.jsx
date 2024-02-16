4eed1dd453c8d0767a7fa04e966134377af2ac2c
Arian <Arianbod@outlook.com>
2024-02-16 16:59:19 +0300

    created

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
-const CocktailCard = () => {
-	return <div>CocktailCard</div>;
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/CocktailCard';
+const CocktailCard = ({ id, image, info, glass, name }) => {
+	return (
+		<Wrapper>
+			<div className='img-container'>
+				<img
+					src={image}
+					alt={name}
+					className='img'
+				/>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default CocktailCard;
