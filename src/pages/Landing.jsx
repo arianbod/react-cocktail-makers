e4c8460715b69311c565d22ee00ed177e2811eef
Arian <Arianbod@outlook.com>
2024-02-16 16:26:32 +0300

    API  established

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,15 @@
+import axios from 'axios';
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+const cocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export const loader = async () => {
+	const searchTerm = ' ';
+	const response = await axios.get(`${cocktailUrl}${searchTerm}`);
+	return { drinks: response.data.drinks, searchTerm };
+};
 const Landing = () => {
+	const data = useLoaderData();
+	console.log(data);
 	return <div>Landing</div>;
 };
 
