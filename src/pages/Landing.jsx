91c3be2eed93b349f3d36d238d8b0db3679a68a2
Arian <Arianbod@outlook.com>
2024-02-16 16:59:57 +0300

    useLoaderData() destructed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,20 @@
 import axios from 'axios';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CocktailList from '../components/CocktailList';
 const cocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 export const loader = async () => {
-	const searchTerm = ' ';
+	const searchTerm = '';
 	const response = await axios.get(`${cocktailUrl}${searchTerm}`);
 	return { drinks: response.data.drinks, searchTerm };
 };
 const Landing = () => {
-	const data = useLoaderData();
-	console.log(data);
-	return <div>Landing</div>;
+	const { drinks } = useLoaderData();
+	return (
+		<>
+			<CocktailList drinks={drinks} />
+		</>
+	);
 };
 
 export default Landing;
