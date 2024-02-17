c707558d5493d7b215bce753ec22b4b962e0e47a
Arian <Arianbod@outlook.com>
2024-02-17 15:28:28 +0300

    Form inputs added

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
 
 const Newsletter = () => {
-	return <div>Newsletter</div>;
+	return (
+		<form className='form'>
+			<h4 style={{ marginBottom: '2rem', textAlign: 'center' }}>
+				our newsletter
+			</h4>
+			{/* name */}
+			<div className='form-row'>
+				<label
+					htmlFor='name'
+					className='form-label'>
+					name
+				</label>
+				<input
+					type='text'
+					defaultValue='John'
+					className='form-input'
+					name='name'
+					id='name'
+				/>
+			</div>
+			<div className='form-row'>
+				<label
+					htmlFor='lastName'
+					className='form-label'>
+					last Name
+				</label>
+				<input
+					type='text'
+					defaultValue='smith'
+					className='form-input'
+					name='lastName'
+					id='lastName'
+				/>
+			</div>
+			<div className='form-row'>
+				<label
+					htmlFor='Email'
+					className='form-label'>
+					Email
+				</label>
+				<input
+					type='text'
+					defaultValue='John@test.com'
+					className='form-input'
+					name='Email'
+					id='Email'
+				/>
+			</div>
+			<button
+				className='btn btn-block'
+				type='submit'
+				style={{ marginTop: '0.5rem' }}>
+				submit
+			</button>
+		</form>
+	);
 };
 
 export default Newsletter;
