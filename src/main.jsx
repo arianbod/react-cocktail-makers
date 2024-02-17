80825ee47afcdf1594bf920f59cdb20be350e1b6
Arian <Arianbod@outlook.com>
2024-02-17 15:28:10 +0300

    Toast added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-  import { ToastContainer, toast } from 'react-toastify';
-	import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-    <ToastContainer position='top-center' autoClose={2000}/>
+		<ToastContainer
+			position='top-center'
+			autoClose={2000}
+		/>
 		<App />
 	</React.StrictMode>
 );
