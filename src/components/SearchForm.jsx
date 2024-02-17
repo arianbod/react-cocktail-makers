1979551e8b02b23508d17791e0cb993158c5a8c6
Arian <Arianbod@outlook.com>
2024-02-17 19:05:11 +0300

    created

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useNavigation, Form } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/SearchForm';
+Wrapper;
 
-const SearchForm = () => {
-	return <div>SearchForm</div>;
+const SearchForm = ({ searchTerm }) => {
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === 'submitting';
+	return (
+		<Wrapper>
+			<Form className='form'>
+				<input
+					type='search'
+					name='search'
+					className='form-input'
+					defaultValue={searchTerm}
+				/>
+				<button
+					className='btn'
+					disabled={isSubmitting}>
+					{isSubmitting ? 'searching...' : 'search'}
+				</button>
+			</Form>
+		</Wrapper>
+	);
 };
 
 export default SearchForm;
